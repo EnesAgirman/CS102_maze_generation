566d79ca32d296f8f554a3a0de3566fff79189b5
Enes Ağırman <99555923+EnesAgirman@users.noreply.github.com>
2022-12-10 10:37:34 +0300

    testing generateMaze()
    function for testing if the generateMaze() function works

--- CS102_Maze_Generation/test.java
+++ CS102_Maze_Generation/test.java
@@ -18,6 +18,29 @@ public class test {
 
         System.out.println("\n@@@@@@@@@@@@@@\nlerolerolerolerolerolero\n@@@@@@@@@@@@@@\n");
 
+        
+
+        System.out.println(myMaze.isMoveLeftPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+        System.out.println(myMaze.isMoveRightPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+
+        System.out.println(myMaze.isMoveUpPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+        System.out.println(myMaze.isMoveDownPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+
+        myMaze.generateMaze();
+
+        System.out.println(" is there a move possible: " + myMaze.movingPossible());
+
+
+        System.out.println(myMaze.currentXLocation);
+        System.out.println(myMaze.currentYLocation);
+
+        System.out.println(myMaze.isMoveLeftPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+        System.out.println(myMaze.isMoveRightPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+
+        System.out.println(myMaze.isMoveUpPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+        System.out.println(myMaze.isMoveDownPossible(myMaze.currentXLocation, myMaze.currentYLocation));
+
+
         // displaying the maze
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
@@ -25,35 +48,6 @@ public class test {
             }
             System.out.println();
         }
-
-        System.out.println(myMaze.isMoveleftPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-        System.out.println(myMaze.isMoveRightPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-
-        System.out.println(myMaze.isMoveUpPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-        System.out.println(myMaze.isMoveDownPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveRight();
-        myMaze.moveDown();
-        myMaze.moveDown();
-
-        System.out.println(myMaze.currentXLocation);
-        System.out.println(myMaze.currentYLocation);
-
-        System.out.println(myMaze.isMoveleftPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-        System.out.println(myMaze.isMoveRightPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-
-        System.out.println(myMaze.isMoveUpPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-        System.out.println(myMaze.isMoveDownPossible(myMaze.currentXLocation, myMaze.currentYLocation));
-
-
         
         /*
         //Integer[] intArray = new Integer[]{ 1,2,3,4,5,6,7,8,9,10 }; 
@@ -70,14 +64,6 @@ public class test {
         System.out.println(XStack.peek());
         System.out.println(YStack.peek());
         */
-
-
-
-
-
-
-
-
 
 
 
